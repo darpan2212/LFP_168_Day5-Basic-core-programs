b6e489d0a88831a7210af04bb73795671e8f910f
Darpan <darpan.madlani@gmail.com>
2022-07-05 21:26:56 +0530

    Swapping values of variables

---
+++ bin/SwapNumber.java
@@ -0,0 +1,18 @@
+public class SwapNumber {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+		
+		System.out.println("a="+a+"\tb="+b);
+
+		/*int temp = a;
+		a = b;
+		b = a;*/
+
+		a = a + b;
+		b = a - b;
+		a = a - b;
+
+		System.out.println("a="+a+"\tb="+b);
+	}
+}
