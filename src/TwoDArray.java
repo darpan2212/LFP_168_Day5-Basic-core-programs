ca6794a0155f97fa8fa06c21cc5ce4a5c2a64c62
Darpan <darpan.madlani@gmail.com>
2022-07-07 20:03:41 +0530

    PrintWriter and OutputStreamWriter

--- src/TwoDArray.java
+++ src/TwoDArray.java
@@ -1,49 +1,85 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class TwoDArray {
 
 	public int[][] twoDarray;
-	public int row,col;
-	
-	public TwoDArray(int row, int col){
+	public int row, col;
+
+	public TwoDArray(int row, int col) {
 		this.row = row;
 		this.col = col;
 		twoDarray = new int[row][col];
 	}
 
 	public static void main(String[] args) {
-	
-		TwoDArray arrObj = new TwoDArray(3,3);
+
+		TwoDArray arrObj = new TwoDArray(3, 3);
 
 		arrObj.initializeArrayValues();
 
 		arrObj.iterateArray();
 	}
-	
+
 	public void iterateArray() {
-		for (int r=0; r<row ;r++) {
+		PrintWriter printWriter = null;
+		try {
+			printWriter = new PrintWriter("matrix.txt");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-			for (int c=0; c<col ; c++) {
-				System.out.print(twoDarray[r][c]+"\t");
+		for (int r = 0; r < row; r++) {
+
+			for (int c = 0; c < col; c++) {
+				printWriter.write(twoDarray[r][c] + "\t");
 			}
-			System.out.print("\n");
-		}	
+			printWriter.write("\n");
+		}
+		printWriter.flush();
+		printWriter.close();
+		
+		OutputStream os;
+		OutputStreamWriter osw;
+		try {
+			os = new FileOutputStream("matrix1.txt");
+			osw = new OutputStreamWriter(os);
+			for (int r = 0; r < row; r++) {
+
+				for (int c = 0; c < col; c++) {
+					osw.write(twoDarray[r][c] + "\t");
+				}
+				osw.write("\n");
+			}
+			osw.flush();
+			osw.close();
+			os.flush();
+			os.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
+
 	public void initializeArrayValues() {
-	
+
 		System.out.println("Enter the array elements");
 		System.out.println("-------------------------");
 		Scanner sc = new Scanner(System.in);
-	
-		for (int r=0; r<row ;r++) {
-		
-			for (int c=0; c<col ; c++) {
-				System.out.print("["+r+"]["+c+"] : ");
+
+		for (int r = 0; r < row; r++) {
+
+			for (int c = 0; c < col; c++) {
+				System.out.print("[" + r + "][" + c + "] : ");
 				twoDarray[r][c] = sc.nextInt();
 			}
-		}	
-	
+		}
+
 		sc.close();
 	}
 
